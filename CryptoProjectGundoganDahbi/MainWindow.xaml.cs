f8d5e7a110a5a1499466641ea178801ade5592b1
Buğra Gündoğan <bugra.gundo@outlook.com>
2022-01-25 15:50:38 +0300

    Initial commit

---
+++ CryptoProjectGundoganDahbi/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptoProjectGundoganDahbi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void m_Search_Box_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void m_Search_Box_KeyDown(object sender, KeyEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void m_Search_Box_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void TotalTextBoxMarket_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void MarketBUYBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void MarketSELLBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CryptoHub
+An application created with the aim of making it easier to control and observe your cryptocurrency investments, also implemented a trader bot. Use it with your own risk.
